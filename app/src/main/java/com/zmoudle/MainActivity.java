11605693ec2bfda4c9df1bbdb6ca8cf2105131d0
854702200 <854702200@qq.com>
2019-11-08 10:59:20 +0800

    模块、组件生成

--- app/src/main/java/com/zmoudle/MainActivity.java
+++ app/src/main/java/com/zmoudle/MainActivity.java
@@ -3,7 +3,9 @@ package com.zmoudle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+
+import com.zmj.zlibrary.Skin.Skin;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Skin.init("室内防水的");
     }
 }
